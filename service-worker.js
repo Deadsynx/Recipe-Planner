8d6110c6a1436dee6c39023b68e6701f9157de8e
Chau7MouYeah <sebastien.czaja57@gmail.com>
2026-08-11 17:21:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+{
+  "name": "Recipe Planner",
+  "short_name": "Recipe Planner",
+  "start_url": "./",
+  "scope": "./",
+  "display": "standalone",
+  "background_color": "#fff4ec",
+  "theme_color": "#fff4ec",
+  "lang": "fr-FR",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
